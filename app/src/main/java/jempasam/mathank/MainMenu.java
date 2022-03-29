6f09f9ae930a77faf5227378b6f6f53dcb1d8057
Samuel Demont <samuel.demont@etu.unice.fr>
2022-03-29 14:42:13 +0200

    MainMenu

---
+++ app/src/main/java/jempasam/mathank/MainMenu.java
@@ -0,0 +1,14 @@
+package jempasam.mathank;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmenu);
+    }
+}
